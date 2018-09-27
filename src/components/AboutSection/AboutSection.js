6a5cc768fee022419d43f6948d6220483924a653
Bohdan <global.site.dev@gmail.com>
2018-09-27 13:48:07 +0300

    Edit About component
    Description...

--- src/components/AboutSection/AboutSection.js
+++ src/components/AboutSection/AboutSection.js
@@ -4,7 +4,7 @@ import classes from './AboutSection.css';
 
 const aboutSection = () => (
   <div className={classes.AboutSectionContainer}>
-    <h2>About Denteez</h2>
+    <h2>About</h2>
     <div className={classes.AboutTextLeft}>Why is it always so difficult to find what you are looking for in dentistry? Whether it is the latest advancement in technology or techniques or simply a review or understanding of the vast amount of products? Perhaps finding someone to just fix your broken equipment or simply hiring new staff or looking for that new job?
     </div>
     <div className={classes.AboutTextRight + ' ' + classes.Clearfix}>Our mission is to give every dental professional the possibility to discuss and share all aspects of their profession, their practice and their business. We aim to make the world of dentistry easy and accessible, so every dental professional can find what they are looking for quickly and easily all in one place.
